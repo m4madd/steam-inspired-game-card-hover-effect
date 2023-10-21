481356e042f3cb7b7481daa0dd4d90573d465b21
m4madd <83618301+m4madd@users.noreply.github.com>
2023-10-21 10:10:33 +0330

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+// Image credits
+// 1. https://pixabay.com/photos/tombstone-graveyard-cemetery-8283591/
+// 2. https://pixabay.com/illustrations/retro-background-the-consignment-4237850/
+// 3. https://pixabay.com/illustrations/fractals-environment-chess-1728594/
+// 4. https://pixabay.com/photos/car-racing-motorsport-racing-car-4394450/
